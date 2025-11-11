9794b13913c12a90d727f802bdea421b903af546
trivenihiremath <triveni1702hiremath@gmail.com>
2025-11-12 01:02:18 +0530

    validation  test case commit

--- src/main/java/com/example/seaprobe/util/Validator.java
+++ src/main/java/com/example/seaprobe/util/Validator.java
@@ -2,14 +2,14 @@ package com.example.seaprobe.util;
 
 import com.example.seaprobe.dto.Coordinate;
 import com.example.seaprobe.dto.Grid;
-
 import java.util.Set;
 
 public class Validator {
     public static  boolean validateCoordinates(Grid grid, Coordinate coordinate) {
-        return false;
+        return coordinate.x()>grid.x1() && coordinate.x()<=grid.x2() &&
+                coordinate.y()> grid.y1() && coordinate.y()<=grid.y2();
     }
     public static boolean notAnObstacle(Set<Coordinate>obstacles, Coordinate coordinate){
-        return  false;
+        return  !obstacles.contains(coordinate);
     }
 }
--- src/test/java/com/example/seaprobe/util/ValidatorTest.java
+++ src/test/java/com/example/seaprobe/util/ValidatorTest.java
@@ -4,10 +4,7 @@ import com.example.seaprobe.dto.Coordinate;
 import com.example.seaprobe.dto.Grid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.HashSet;
 import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -21,9 +18,22 @@ class ValidatorTest {
     }
     @Test
     void testNotAnObstacle_false(){
-        Set<Coordinate> obstacle = new HashSet<>();
-        obstacle.add(new Coordinate(2,2));
-        Coordinate coordinate = new Coordinate(3,3);
-        assertFalse(Validator.notAnObstacle(obstacle, coordinate));
+        Set<Coordinate> obstacle = Set.of(new Coordinate(1,2),
+                new Coordinate(4,2));
+        Coordinate invalidCoordinate = new Coordinate(4,2);
+        assertFalse(Validator.notAnObstacle(obstacle, invalidCoordinate));
+    }
+    @Test
+    void testValidCoordinates_true(){
+        Grid grid = new Grid(0,5,0,5);
+        Coordinate coordinate = new Coordinate(2,3);
+        assertTrue(Validator.validateCoordinates(grid, coordinate));
+    }
+    @Test
+    void testNotAnObstacle_true(){
+        Set<Coordinate> obstacle = Set.of(new Coordinate(1,2),
+                new Coordinate(4,2));
+        Coordinate validCoordinate = new Coordinate(2,2);
+        assertTrue(Validator.notAnObstacle(obstacle, validCoordinate));
     }
 }
